4764ad4ec7ac4df29afc555f2836232d2b45fba7
nunu111 <nunu111za@hotmail.com>
2022-08-04 14:12:26 +0700

    เสียใจทำไม่ทัน

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const photo = document.getElementById("img-profile");
+const all = document.getElementById("div-user-card");
+const btng = document.getElementById("btn-random");
+const email = document.getElementById("p-email");
+const name = document.getElementById("p-name");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  photo.src = resp.data.results[0].picture.large;
+  email.innerHTML = resp.data.results[0].email;
+  name.innerHTML =
+    resp.data.results[0].title +
+    resp.data.results[0].first +
+    resp.data.results[0].last;
+  all.style.display = "";
 }
+
+callApi();
+
+photo.onload = () => {
+  all.style.display = "none";
+  all.style.display = "";
+};
